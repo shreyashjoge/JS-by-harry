eca7f6ea4ee269d2f0c37c9a15f3c339ef618863
shreyashjoge <153541753+shreyashjoge@users.noreply.github.com>
2025-02-18 09:31:46 +0000

    variables

---
+++ learningJS/02_variables.js
@@ -0,0 +1,14 @@
+console.log("dsaw")
+
+let a = 67 
+// here variable is "a" and a contains 67
+// let b = 100
+// here variable is "b"
+
+console.log(a);
+
+// let 5dsaw = 8 // not allowed
+
+// let var = 8
+
+ 
