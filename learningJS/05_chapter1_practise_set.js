93db00cdd676cf3d40e964b77bff57910882c579
shreyashjoge <153541753+shreyashjoge@users.noreply.github.com>
2025-02-18 12:18:02 +0000

    practise set com

--- learningJS/05_chapter1_practise_set.js
+++ learningJS/05_chapter1_practise_set.js
@@ -11,19 +11,29 @@ console.log(typeof(a+b));
 
 // Q3
 const a1 = {
-    "name" : "dsaw"
-    "section" : 4
-    "isPrincipal" : false 
+    name : "miles",
+    section : 1,
+    isPrincipal : false
 }
 
-a1 = 54
+// a1 = 56
 
-// Q4
-a1["friend"] = "hirani"
-a1["name"] = "tree"
- console.log(a1);
- 
- //Q5
- const dict = {
-    
- }
+//Q4
+a1["friend"] = "more"
+a1["name"] = "joge"
+console.log(a1);
+
+//Q5
+const dict = {
+    lion : "Animal",
+    Ephemeral : "Short-lived",
+    Obfuscate : "Confuse",
+    Quixotic : "Idealistic, impractical",
+    Lugubrious : "Sad",
+    Mellifluous : "Sweet-sound"
+}
+console.log(dict);
+
+console.log(dict.Obfuscate); // log(dict[obfuscate]) or log(log.Obfuscate) / answer will be same 
+
+
